21657adfa8100e89731bd7dd7be312eaac4af7bd
윤민석 <ykmvm147@naver.com>
2023-06-07 21:31:01 +0900

    feat: 장바구니 페이지 구현 완료 및 메인,상품상세 페이지 텍스트 추가 완료

---
+++ pages/cart/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Cart: NextPage = () => {
+  return <>장바구니 페이지</>;
+};
+
+export default Cart;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <></>;
+  return <>메인 페이지</>;
 };
 
 export default Home;
