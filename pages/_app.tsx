63bade14312f563d019d73ae04cd4e0b7d9bf489
윤민석 <ykmvm147@naver.com>
2023-06-07 21:36:31 +0900

    feat: _app 페이지 구현 및 styles 설정 완료

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import "../styles/reset.css";
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>e-maxmall</title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=10,user-scalable=yes"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
