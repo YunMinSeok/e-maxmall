ad0607dc52442f4d30d31fed827c648d448873db
윤민석 <ykmvm147@naver.com>
2023-06-14 23:40:19 +0900

    feat: product 데이터 추가 완료

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -38,6 +38,41 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         price: "5000",
         score: "100",
       },
+      {
+        item_no: 6,
+        item_name: "이미지 6",
+        detail_image_url: "",
+        price: "6000",
+        score: "600",
+      },
+      {
+        item_no: 7,
+        item_name: "이미지 7",
+        detail_image_url: "",
+        price: "7000",
+        score: "700",
+      },
+      {
+        item_no: 8,
+        item_name: "이미지 8",
+        detail_image_url: "",
+        price: "8000",
+        score: "500",
+      },
+      {
+        item_no: 9,
+        item_name: "이미지 9",
+        detail_image_url: "",
+        price: "9000",
+        score: "400",
+      },
+      {
+        item_no: 10,
+        item_name: "이미지 10",
+        detail_image_url: "",
+        price: "10000",
+        score: "100",
+      },
     ],
   });
 }
