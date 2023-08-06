bf0f9d43e21c4a58b2408ad6bf8961809d442feb
윤민석 <ykmvm147@naver.com>
2023-08-06 17:03:18 +0900

    feat: 로그인 페이지 UI 구현 완료

--- src/styles/login/login.ts
+++ src/styles/login/login.ts
@@ -1,7 +1,44 @@
 import styled from "@emotion/styled";
 
 export const LoginWrap = styled.div`
-  width: 1050px;
+  min-width: 290px;
+  max-width: 460px;
   margin: 0px auto;
   padding: 40px 0px;
+  .member-field {
+    margin-top: 10px;
+    border: 1px solid #ccc;
+    .member-input-field {
+      position: relative;
+      .member-input {
+        width: 100%;
+        height: 48px;
+        padding: 16px 0 12px;
+        border: 0 none;
+        margin: 0;
+        box-sizing: border-box;
+        background: none transparent;
+        font-family: dotum, sans-serif;
+        font-size: 14px;
+        line-height: 20px;
+        color: #111;
+        font-weight: 700;
+        text-indent: 12px;
+      }
+    }
+  }
+
+  .login-button-field {
+    border: 0;
+    .login-button {
+      width: 100%;
+      background: #346aff;
+      padding: 12.5px 0;
+      border-radius: 4px;
+      font-size: 16px;
+      box-shadow: none;
+      line-height: 19px;
+      color: #fff;
+    }
+  }
 `;
--- templates/login/LoginTemplate.tsx
+++ templates/login/LoginTemplate.tsx
@@ -7,6 +7,21 @@ const LoginTemplate = () => {
   return (
     <LoginWrap>
       <Header title={"로그인"} isLinkShow={false} />
+      <div className="login-content">
+        <div className="member-field">
+          <div className="member-input-field">
+            <input className="member-input" placeholder="아이디(이메일)" />
+          </div>
+        </div>
+        <div className="member-field">
+          <div className="member-input-field">
+            <input className="member-input" placeholder="비밀번호" />
+          </div>
+        </div>
+        <div className="member-field login-button-field">
+          <button className="login-button">로그인</button>
+        </div>
+      </div>
     </LoginWrap>
   );
 };
