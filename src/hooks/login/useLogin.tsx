973bf40ae6f08fb1145bb214283d97b4480e141d
윤민석 <ykmvm147@naver.com>
2023-08-07 15:36:58 +0900

    feat: login시 빈칸시 valid 체크 완ㄹ

--- src/hooks/login/useLogin.tsx
+++ src/hooks/login/useLogin.tsx
@@ -1,3 +1,4 @@
+import { RefObject } from "react";
 // util
 import { setCookie } from "@util/cookie";
 
@@ -6,9 +7,19 @@ import { getMe } from "@api/user";
 
 export const useLogin = () => {
   // 로그인 버튼 클릭 사용되는 함수
-  const handleLoginSubmit = async () => {
+  const handleLoginSubmit = async (
+    email: RefObject<HTMLInputElement>,
+    password: RefObject<HTMLInputElement>,
+  ) => {
+    // 이메일, 패스워드 빈값시 return
+    if (!email.current!.value && !password.current!.value) {
+      alert("이메일과 패스워드를 다시 한번 확인해주세요.");
+      return;
+    }
+    // user 데이터 가져오기
     const user = await getMe();
     if (!!user) {
+      // 쿠키 설정
       setCookie("user", user.name);
     }
   };
--- templates/login/LoginTemplate.tsx
+++ templates/login/LoginTemplate.tsx
@@ -3,8 +3,11 @@ import Header from "@components/common/Header";
 import { useLogin } from "@hooks/login/useLogin";
 // css
 import { LoginWrap } from "@styles/login/login";
+import { useRef } from "react";
 
 const LoginTemplate = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
   const { handleLoginSubmit } = useLogin();
   return (
     <LoginWrap>
@@ -12,16 +15,20 @@ const LoginTemplate = () => {
       <div className="login-content">
         <div className="member-field">
           <div className="member-input-field">
-            <input className="member-input" placeholder="아이디(이메일)" />
+            <input ref={emailRef} className="member-input" placeholder="아이디(이메일)" />
           </div>
         </div>
         <div className="member-field">
           <div className="member-input-field">
-            <input className="member-input" placeholder="비밀번호" />
+            <input ref={passwordRef} className="member-input" placeholder="비밀번호" />
           </div>
         </div>
         <div className="member-field login-button-field">
-          <button type="submit" className="login-button" onClick={handleLoginSubmit}>
+          <button
+            type="submit"
+            className="login-button"
+            onClick={() => handleLoginSubmit(emailRef, passwordRef)}
+          >
             로그인
           </button>
         </div>
