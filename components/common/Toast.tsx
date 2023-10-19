a7b294ce60ed5fa66c06fc1456db7be1965084ce
윤민석 <ykmvm147@naver.com>
2023-10-19 22:45:22 +0900

    feat: icon 연동 오나료

--- components/common/Toast.tsx
+++ components/common/Toast.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
+// image
+import Error from "@images/icon/error.png";
+import Info from "@images/icon/info.png";
+import Success from "@images/icon/success.png";
+import Warning from "@images/icon/warning.png";
 // type
 import { ToastProps } from "@type/common/Toast";
-
 // css
 import { Container, IconContainer, Message, CloseButton } from "@styles/common/toast";
-import { useState } from "react";
 
 const Toast = ({ message, type }: ToastProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false); // recoil로 변경하자
 
   function getIcon() {
-    if (type === "success") return <Icon.Success />;
-    if (type === "warning") return <Icon.Warning />;
-    if (type === "error") return <Icon.Error />;
-    if (type === "info") return <Icon.Info />;
+    if (type === "success") return <Success />;
+    if (type === "warning") return <Warning />;
+    if (type === "error") return <Error />;
+    if (type === "info") return <Info />;
   }
 
   return (
