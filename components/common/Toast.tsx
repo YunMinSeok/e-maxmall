26f6639f73b73115ae72a2e4bf49c9ce9c3dddd1
윤민석 <ykmvm147@naver.com>
2023-10-19 22:42:04 +0900

    feat: icon download 완료

--- components/common/Toast.tsx
+++ components/common/Toast.tsx
@@ -7,6 +7,14 @@ import { useState } from "react";
 
 const Toast = ({ message, type }: ToastProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false); // recoil로 변경하자
+
+  function getIcon() {
+    if (type === "success") return <Icon.Success />;
+    if (type === "warning") return <Icon.Warning />;
+    if (type === "error") return <Icon.Error />;
+    if (type === "info") return <Icon.Info />;
+  }
+
   return (
     <Container>
       <IconContainer>{getIcon()}</IconContainer>
