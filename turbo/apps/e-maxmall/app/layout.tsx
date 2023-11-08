51fe0319c42d40af91f663aff4f812530a6e3482
윤민석 <ykmvm147@naver.com>
2023-11-08 20:09:28 +0900

    feat: layout 오류 해결 완료 (html태그 없음 오류)

--- turbo/apps/e-maxmall/app/layout.tsx
+++ turbo/apps/e-maxmall/app/layout.tsx
@@ -16,21 +16,26 @@ import AsyncWrapper from "@components/common/AsyncWrapper";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <HydrationBoundary state={{ dehydratedState: dehydrate(queryClient) }}>
-          <Head>
-            <title>e-maxmall</title>
-            <meta charSet="UTF-8" />
-            <meta
-              name="viewport"
-              content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=10,user-scalable=yes"
-            />
-          </Head>
-          <AsyncWrapper>{children}</AsyncWrapper>
-          <ReactQueryDevtools buttonPosition={"bottom-left"} initialIsOpen={false} />
-        </HydrationBoundary>
-      </QueryClientProvider>
-    </RecoilRoot>
+    <html>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <HydrationBoundary state={{ dehydratedState: dehydrate(queryClient) }}>
+            <Head>
+              <title>e-maxmall</title>
+              <meta charSet="UTF-8" />
+              <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=10,user-scalable=yes"
+              />
+            </Head>
+            <body>
+              <AsyncWrapper>{children}</AsyncWrapper>
+            </body>
+
+            <ReactQueryDevtools buttonPosition={"bottom-left"} initialIsOpen={false} />
+          </HydrationBoundary>
+        </QueryClientProvider>
+      </RecoilRoot>
+    </html>
   );
 }
