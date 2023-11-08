6c79ee748ae8c522d8d26976014b25b3f13667f9
윤민석 <ykmvm147@naver.com>
2023-11-08 20:18:39 +0900

    feat: html 가져오지 못하는 오류 해결 완료

--- turbo/apps/e-maxmall/app/layout.tsx
+++ turbo/apps/e-maxmall/app/layout.tsx
@@ -16,26 +16,25 @@ import AsyncWrapper from "@components/common/AsyncWrapper";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <RecoilRoot>
-        <QueryClientProvider client={queryClient}>
-          <HydrationBoundary state={{ dehydratedState: dehydrate(queryClient) }}>
-            <Head>
-              <title>e-maxmall</title>
-              <meta charSet="UTF-8" />
-              <meta
-                name="viewport"
-                content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=10,user-scalable=yes"
-              />
-            </Head>
-            <body>
+    <html lang="en">
+      <body>
+        <RecoilRoot>
+          <QueryClientProvider client={queryClient}>
+            <HydrationBoundary state={{ dehydratedState: dehydrate(queryClient) }}>
+              <Head>
+                <title>e-maxmall</title>
+                <meta charSet="UTF-8" />
+                <meta
+                  name="viewport"
+                  content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=10,user-scalable=yes"
+                />
+              </Head>
               <AsyncWrapper>{children}</AsyncWrapper>
-            </body>
-
-            <ReactQueryDevtools buttonPosition={"bottom-left"} initialIsOpen={false} />
-          </HydrationBoundary>
-        </QueryClientProvider>
-      </RecoilRoot>
+              <ReactQueryDevtools buttonPosition={"bottom-left"} initialIsOpen={false} />
+            </HydrationBoundary>
+          </QueryClientProvider>
+        </RecoilRoot>
+      </body>
     </html>
   );
 }
