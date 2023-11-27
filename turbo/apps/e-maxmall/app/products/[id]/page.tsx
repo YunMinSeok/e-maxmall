efc31c053d58270acabb08eb392a5b09e3494f8c
윤민석 <ykmvm147@naver.com>
2023-11-27 19:02:46 +0900

    feat: dynamic route 값 가져오는거 변경 완료

--- turbo/apps/e-maxmall/app/products/[id]/page.tsx
+++ turbo/apps/e-maxmall/app/products/[id]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { NextPage, GetStaticPaths, GetStaticProps } from "next";
 import { dehydrate } from "@tanstack/react-query";
 
 // templates
@@ -11,7 +9,6 @@ import { ProductItemVO } from "@type/product/product";
 // query
 import { queryClient } from "@query/queryClient";
 import { queryKeys, commonOptions } from "@query/constant";
-
 // api
 import { getProductDetail, getAllProduct } from "@api/product";
 
@@ -19,37 +16,26 @@ interface ProductDetailType {
   product: ProductItemVO;
 }
 
-const ProductDetail: NextPage<ProductDetailType> = ({ product }) => {
-  return <ProductDetailTemplate product={product} />;
-};
-
-export default ProductDetail;
-
-export const getStaticPaths: GetStaticPaths = async () => {
+// get product id
+export async function generateStaticParams() {
   const productIds = await getAllProduct();
 
-  // paths 배열에 동적 경로 객체들을 생성
-  const paths = productIds.productItems.map((product: ProductItemVO) => ({
+  return productIds.productItems.map((product: ProductItemVO) => ({
     params: { id: String(product.item_no) },
   }));
-
-  return { paths, fallback: false }; // fallback 옵션 설정
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+}
+// get product detail data
+async function fetchData(id: string) {
   try {
-    const page = params?.id as string;
+    const page = id;
     const product = await queryClient.fetchQuery({
       queryKey: [queryKeys.product, page],
       queryFn: () => getProductDetail(page),
       ...commonOptions,
     });
-
     return {
-      props: {
-        product: product,
-        dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-      },
+      product: product,
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
     };
   } catch (err) {
     return {
@@ -61,4 +47,53 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   } finally {
     queryClient.clear();
   }
+}
+
+const ProductDetail = async ({ params }: { params: { id: string } }) => {
+  console.log(params);
+  const product = await fetchData(params.id);
+  console.log(product);
+  if (!product.product) return;
+
+  return <ProductDetailTemplate product={product.product} />;
 };
+
+export default ProductDetail;
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const productIds = await getAllProduct();
+
+//   // paths 배열에 동적 경로 객체들을 생성
+//   const paths = productIds.productItems.map((product: ProductItemVO) => ({
+//     params: { id: String(product.item_no) },
+//   }));
+
+//   return { paths, fallback: false }; // fallback 옵션 설정
+// };
+
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   try {
+//     const page = params?.id as string;
+//     const product = await queryClient.fetchQuery({
+//       queryKey: [queryKeys.product, page],
+//       queryFn: () => getProductDetail(page),
+//       ...commonOptions,
+//     });
+
+//     return {
+//       props: {
+//         product: product,
+//         dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+//       },
+//     };
+//   } catch (err) {
+//     return {
+//       redirect: {
+//         destination: "/products",
+//         statusCode: 307,
+//       },
+//     };
+//   } finally {
+//     queryClient.clear();
+//   }
+// };
