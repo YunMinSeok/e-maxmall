e8af924d829de3e2d754ecb98e2a13f7c9d39ab3
윤민석 <ykmvm147@naver.com>
2023-11-30 23:06:15 +0900

    feat: product id 필요없는 부분 삭제 완료

--- turbo/apps/e-maxmall/app/products/[id]/page.tsx
+++ turbo/apps/e-maxmall/app/products/[id]/page.tsx
@@ -20,17 +20,18 @@ interface ProductDetailType {
 export async function generateStaticParams() {
   const productIds = await getAllProduct();
 
+  console.log(productIds);
   return productIds.productItems.map((product: ProductItemVO) => ({
     params: { id: String(product.item_no) },
   }));
 }
 // get product detail data
 async function fetchData(id: string) {
+  console.log("id", id);
   try {
-    const page = id;
     const product = await queryClient.fetchQuery({
-      queryKey: [queryKeys.product, page],
-      queryFn: () => getProductDetail(page),
+      queryKey: [queryKeys.product, id],
+      queryFn: () => getProductDetail(id),
       ...commonOptions,
     });
     return {
@@ -52,48 +53,10 @@ async function fetchData(id: string) {
 const ProductDetail = async ({ params }: { params: { id: string } }) => {
   console.log(params);
   const product = await fetchData(params.id);
-  console.log(product);
+
   if (!product.product) return;
 
   return <ProductDetailTemplate product={product.product} />;
 };
 
 export default ProductDetail;
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const productIds = await getAllProduct();
-
-//   // paths 배열에 동적 경로 객체들을 생성
-//   const paths = productIds.productItems.map((product: ProductItemVO) => ({
-//     params: { id: String(product.item_no) },
-//   }));
-
-//   return { paths, fallback: false }; // fallback 옵션 설정
-// };
-
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   try {
-//     const page = params?.id as string;
-//     const product = await queryClient.fetchQuery({
-//       queryKey: [queryKeys.product, page],
-//       queryFn: () => getProductDetail(page),
-//       ...commonOptions,
-//     });
-
-//     return {
-//       props: {
-//         product: product,
-//         dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-//       },
-//     };
-//   } catch (err) {
-//     return {
-//       redirect: {
-//         destination: "/products",
-//         statusCode: 307,
-//       },
-//     };
-//   } finally {
-//     queryClient.clear();
-//   }
-// };
