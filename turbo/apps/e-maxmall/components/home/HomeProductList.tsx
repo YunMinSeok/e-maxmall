73655455f6e7f059a6ae963f671ea5756483b41e
윤민석 <ykmvm147@naver.com>
2024-01-09 23:29:27 +0900

    feat: 이미지 * 로 한번에 배출하는거 적용중

--- turbo/apps/e-maxmall/components/home/HomeProductList.tsx
+++ turbo/apps/e-maxmall/components/home/HomeProductList.tsx
@@ -1,10 +1,37 @@
+import { StaticImageData } from "next/image";
+import { useRef } from "react";
+// components
+import ShowCaseCard from "@components/home/ShowCase";
+// hooks
+import { useMouseEvent } from "@hooks/home/useMouseEvent";
 // image
-import ProductImage from "@images/product_image_1.jpg";
+import * as ProductImages from "@images/index";
 // styles
 import { ProductListWrap } from "@styles/home/home";
 
 const HomeProductList = () => {
-  return <ProductListWrap></ProductListWrap>;
+  const container = useRef<HTMLDivElement>(null);
+  const overlay = useRef<HTMLDivElement>(null);
+
+  const { handleMouseMove, handleMouseOut } = useMouseEvent(container, overlay);
+
+  console.log(ProductImages);
+  return (
+    <ProductListWrap>
+      {/* {result.map((productImage: StaticImageData) => {
+        console.log(productImage);
+        return (
+          <ShowCaseCard
+            cardImage={productImage.src}
+            containerRef={container}
+            overlayRef={overlay}
+            mouseMoveEvent={handleMouseMove}
+            mouseOutEvent={handleMouseOut}
+          />
+        );
+      })} */}
+    </ProductListWrap>
+  );
 };
 
 export default HomeProductList;
