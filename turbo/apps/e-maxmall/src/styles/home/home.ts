7f8b5403be20c60b6e1db8306ca1e3f89a2a094c
윤민석 <ykmvm147@naver.com>
2024-01-11 23:05:16 +0900

    feat: overlay 초반 무지개 뜨는거 해결 완료

--- turbo/apps/e-maxmall/src/styles/home/home.ts
+++ turbo/apps/e-maxmall/src/styles/home/home.ts
@@ -104,7 +104,7 @@ export const ShowCase = styled.div<ShowCaseType>`
       rgba(132, 50, 255, 0.6) 50%,
       transparent 54%
     );
-    filter: brightness(1.1) opacity(0.8);
+    filter: brightness(1.1) opacity(0);
     mix-blend-mode: color-dodge;
     background-size: 150% 150%;
     background-position: 100%;
